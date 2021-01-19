aa37d3327c7b8c478b70ebfea0043845c8bbcebd
Muni10 <sten.ivar.rokke@gmail.com>
2021-01-19 11:37:44 +0100

    Added file exists check

--- db-backup.sh
+++ db-backup.sh
@@ -13,19 +13,21 @@ for namespace in ${all_db_namespaces}; do
   project_id=$(kubectl get namespace "$namespace" -o jsonpath='{.metadata.annotations.cnrm\.cloud\.google\.com/project-id}')
   gcloud auth activate-service-account --key-file /credentials/saKey
   gcloud config set project "$project_id"
-  echo "project_id: ${project_id}"
   for db in $dbs; do
     instance=$(kubectl get sqldatabase "$db" -n "$namespace" --no-headers -o custom-columns=":spec.instanceRef.name")
     verifyInstance=$(kubectl get sqlinstance -n "$namespace" "$instance" >/dev/null 2>&1)
     if [ $? != 0 ]; then
       echo "spec.instanceRef.name in database $db does not exist. Skipping instance $instance..."
     else
-      echo "Backing up instance: $instance"
+        echo "$(date +%H%M%S): Backing up instance $instance"
       service_account_email=$(kubectl get sqlinstance "$instance" -n "$namespace" --no-headers -o custom-columns=":status.serviceAccountEmailAddress")
-      dump_file_name="$(date +%Y%m%d%H%M%S)_${instance}_${project_id}"
+      dump_file_name="$(date +%Y%m%d)_${instance}
       gsutil iam ch serviceAccount:"${service_account_email}":objectCreator gs://"$BUCKET_NAME"
       # TODO: Consider using --offload for less disruption
-      gcloud sql export sql "${instance}" gs://"$BUCKET_NAME"/"$namespace"/"$dump_file_name" --database="$db"
+      gsutil -q stat gs://"$BUCKET_NAME"/"$namespace"/"$dump_file_name"
+      if [ $? != 0 ]; then
+        gcloud sql export sql "${instance}" gs://"$BUCKET_NAME"/"$namespace"/"$dump_file_name" --database="$db"
+      fi
     fi
   done
 done
